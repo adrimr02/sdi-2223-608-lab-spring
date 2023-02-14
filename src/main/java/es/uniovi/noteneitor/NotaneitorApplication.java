fbcf4966b1a376459efc5eb8fe1149bf0a562584
adrimr02 <adrianmartinezr2002@gmail.com>
2023-02-14 11:56:39 +0100

    SDI-2223-608-2.2-Trabajando con Controladores.

--- src/main/java/es/uniovi/noteneitor/NotaneitorApplication.java
+++ src/main/java/es/uniovi/noteneitor/NotaneitorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sdi608SpringApplication {
+public class NotaneitorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Sdi608SpringApplication.class, args);
+        SpringApplication.run(NotaneitorApplication.class, args);
     }
 
 }
---
+++ src/main/java/es/uniovi/noteneitor/controllers/MarkController.java
@@ -0,0 +1,24 @@
+package es.uniovi.noteneitor.controllers;
+
+import es.uniovi.noteneitor.entities.Mark;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MarkController {
+
+    @RequestMapping("/mark/list")
+    public String getList() {
+        return "Getting list";
+    }
+
+    @RequestMapping(value = "/mark/add", method = RequestMethod.POST)
+    public String setMark(@ModelAttribute Mark mark) {
+        return "Adding " + mark.getDescription() + " with score " + mark.getScore() + " and Id " + mark.getId();
+    }
+    @RequestMapping("/mark/details/{id}")
+    public String getDetail(@PathVariable long id) {
+        return "Getting Details => " + id;
+    }
+
+
+}
