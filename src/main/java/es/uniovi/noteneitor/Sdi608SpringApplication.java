55b7b9ff628ac8c5e770f40b1e9d472305a5d2ba
adrimr02 <adrianmartinezr2002@gmail.com>
2023-02-14 11:27:59 +0100

    SDI-2223-608-2.1-Create Spring Project.

---
+++ src/main/java/es/uniovi/noteneitor/Sdi608SpringApplication.java
@@ -0,0 +1,13 @@
+package es.uniovi.noteneitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi608SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi608SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/uniovi/noteneitor/Sdi608SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uniovi.noteneitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi608SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
