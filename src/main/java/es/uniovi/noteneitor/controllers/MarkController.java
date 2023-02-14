c731182bec8df0e323557e5df9b9711fd5ba1960
adrimr02 <adrianmartinezr2002@gmail.com>
2023-02-14 12:08:16 +0100

    SDI-2223-608-2.3-Beans, servicios e inyección de dependencias.

--- src/main/java/es/uniovi/noteneitor/controllers/MarkController.java
+++ src/main/java/es/uniovi/noteneitor/controllers/MarkController.java
@@ -1,23 +1,35 @@
 package es.uniovi.noteneitor.controllers;
 
 import es.uniovi.noteneitor.entities.Mark;
+import es.uniovi.noteneitor.services.MarksService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MarkController {
 
+    @Autowired
+    private MarksService marksService;
+
     @RequestMapping("/mark/list")
     public String getList() {
-        return "Getting list";
+        return marksService.getMarks().toString();
     }
 
     @RequestMapping(value = "/mark/add", method = RequestMethod.POST)
     public String setMark(@ModelAttribute Mark mark) {
-        return "Adding " + mark.getDescription() + " with score " + mark.getScore() + " and Id " + mark.getId();
+        marksService.addMark(mark);
+        return "Ok";
+    }
+
+    @RequestMapping("/mark/delete/{id}")
+    public String deleteMark(@PathVariable Long id) {
+        marksService.deleteMark(id);
+        return "Ok";
     }
     @RequestMapping("/mark/details/{id}")
     public String getDetail(@PathVariable long id) {
-        return "Getting Details => " + id;
+        return marksService.getMark(id).toString();
     }
 
 
