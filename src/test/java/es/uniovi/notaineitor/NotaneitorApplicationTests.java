b6a20d4a87e7422211acd4158ec9bf5475869017
adrimr02 <adrianmartinezr2002@gmail.com>
2023-03-04 16:49:45 +0100

    SDI-2223-608-5.4-Casos Uso VistaHome.

--- src/test/java/es/uniovi/notaineitor/NotaneitorApplicationTests.java
+++ src/test/java/es/uniovi/notaineitor/NotaneitorApplicationTests.java
@@ -1,11 +1,17 @@
 package es.uniovi.notaineitor;
 
+import es.uniovi.notaineitor.pageobjects.PO_HomeView;
+import es.uniovi.notaineitor.pageobjects.PO_Properties;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
+import java.util.List;
+
+@SpringBootTest(webEnvironment=WebEnvironment.MOCK)
 @TestMethodOrder( MethodOrderer.OrderAnnotation.class )
 class NotaneitorApplicationTests {
     static String PathFirefox = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
@@ -35,34 +41,38 @@ class NotaneitorApplicationTests {
 
     @Test
     @Order(1)
-    void PR01(){}
+    void PR01A() {
+        PO_HomeView.checkWelcomeToPage(driver, PO_Properties.getSPANISH());
+    }
     @Test
     @Order(2)
-    void PR02(){}
+    void PR01B() {
+        List<WebElement> welcomeMessageElement = PO_HomeView.getWelcomeMessageText(driver,
+                PO_Properties.getSPANISH());
+        Assertions.assertEquals(welcomeMessageElement.get(0).getText(),
+                PO_HomeView.getP().getString("welcome.message", PO_Properties.getSPANISH()));
+    }
+
+    //PR02. Opción de navegación. Pinchar en el enlace Registro en la página home
     @Test
     @Order(3)
-    void PR03(){}
+    public void PR02() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+    }
+    //PR03. Opción de navegación. Pinchar en el enlace Identifícate en la página home
     @Test
     @Order(4)
-    void PR04(){}
+    public void PR03() {
+        PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+    }
+
+    //PR04. Opción de navegación. Cambio de idioma de Español a Inglés y vuelta a Español
     @Test
     @Order(5)
-    void PR05(){}
-    @Test
-    @Order(6)
-    void PR06(){}
-    @Test
-    @Order(7)
-    void PR07(){}
-    @Test
-    @Order(8)
-    void PR08(){}
-    @Test
-    @Order(9)
-    void PR09(){}
-    @Test
-    @Order(10)
-    void PR10(){}
+    public void PR04() {
+        PO_HomeView.checkChangeLanguage(driver, "btnSpanish", "btnEnglish",
+                PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+    }
 
     //Después de cada prueba se borran las cookies del navegador
     @AfterEach
